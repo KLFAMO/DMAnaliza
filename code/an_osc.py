47d4e6e8c87b30e5e2492520b20c69c6493350c3
Piotr Morzyński <pmorzynski@umk.pl>
2023-09-17 11:19:32 +0200

    changed name of s, etaum; removing some comments and sume minor changes

--- code/an_osc.py
+++ code/an_osc.py
@@ -10,7 +10,7 @@ import time
 import multiprocessing
 import parameters as par
 
-etaum = 0
+etau_mjd = 0
 
 # 0: K, 1: std
 sd = [0]*len(par.labs)
@@ -19,9 +19,9 @@ def fu(dx,A,sh):
     return [f(x,A,sh) for x in dx]
 
 def f(x,A,sh):   
-    global etaum
+    global etau_mjd
     rx = int(x)
-    return A*np.sin(etaum*(x-rx)+sh)*ssf_osc(Om)
+    return A*np.sin(etau_mjd*(x-rx)+sh)*ssf_osc(Om)
 
 def sigf(dx):
     return [ sd[int(x)] for x in dx]
@@ -57,33 +57,30 @@ def calc_single(mjd, om):
     :Changes:
         2023-09-15 (Piotr Morzyński): First version
     """
-    global etaum
+    global etau_mjd
     global sd
 
-    etau = om  # rad/s
-    etaum = etau*86400
-    end_mjd = mjd+0.05
-    durm = end_mjd-mjd
+    etau_s = 3*2*np.pi/om  # [s]
+    etau_mjd = etau_s/86400.
+    end_mjd = mjd+etau_mjd
     datx=[]
     daty=[]
-
     cnt = 0
     clocks = 0
     # capture data for labs for given mjd, om
     for lab in par.labs:
-        if lab in d.keys():
-            s = d[lab].getrange(mjd,end_mjd)
+        if lab in labs_data.keys():
+            data_serie = labs_data[lab].getrange(mjd,end_mjd)
             
-            if s != None and len(s.dtab)==1:
-                if s.dtab[0].mjd_tab[-1]-s.dtab[0].mjd_tab[0] >= 0.95*durm:
-                    #s.rm_dc()
-                    s.rm_drift_each()
-                    #s.plot()
-                    datx.append(s.mjd_tab() - (mjd) + par.lnum[lab])
-                    daty.append(s.val_tab())
+            if data_serie != None and len(data_serie.dtab)==1:
+                # TODO: add checking that there is resonable uptime
+                if data_serie.dtab[0].mjd_tab[-1]-data_serie.dtab[0].mjd_tab[0] >= 0.75*etau_mjd:
+                    data_serie.rm_drift()
+                    datx.append(data_serie.mjd_tab() - (mjd) + par.lnum[lab])
+                    daty.append(data_serie.val_tab())
                     cnt = cnt+1
                     clocks = clocks + (1 << par.lnum[lab])
-                    sd[par.lnum[lab]]=s.std()
+                    sd[par.lnum[lab]]=data_serie.std()
 
     if cnt>2:
             datx = np.concatenate(datx)
@@ -92,14 +89,6 @@ def calc_single(mjd, om):
             try:
                 popt, pcov = scp.curve_fit(fu, datx, daty,  
                             sigma=sig, absolute_sigma=True )
-                #print('POPT:  ', popt)
-                #print('PCOV:  ', pcov)
-                #plt.plot([x-int(x) for x in datx], daty, 
-                #        [x-int(x) for x in datx], fu(datx,*popt), 
-                        #[x-int(x) for x in datx], sig
-                #        )
-                #plt.plot(datx, daty, datx, fu(datx,*popt))
-                #plt.show()
             except:
                 return None
             return [popt[0], pcov[0,0]**0.5, pcov[1,1]**0.5, clocks]
@@ -107,20 +96,17 @@ def calc_single(mjd, om):
             return None
 
 #reading data from npy files
-d = dict()
+labs_data = dict()
 for lab in par.labs:
-    print('\n'+lab)
-    print(os.path.isfile( str( progspath / (r'DMAnaliza/data/d_prepared/d_' +lab+'_'+par.camp+'.npy') ) ))
     if os.path.isfile( str( progspath / (r'DMAnaliza/data/d_prepared/d_' +lab+'_'+par.camp+'.npy') ) ):
-        #print('\n'+lab)
-        d[lab] = tls.MTSerie(lab, color=par.inf[lab]['col'])
-        d[lab].add_mjdf_from_file(
+        labs_data[lab] = tls.MTSerie(lab, color=par.inf[lab]['col'])
+        labs_data[lab].add_mjdf_from_file(
             str( progspath / (r'DMAnaliza/data/d_prepared/d_' +lab+'_'+par.camp+'.npy') )   )
-        d[lab].split(min_gap=12)
+        labs_data[lab].split(min_gap=12)
         #d[lab].rm_dc_each()
         #d[lab].high_gauss_filter_each(stddev=350)
         #d[lab].rm_drift_each()
-        d[lab].alphnorm(atom=par.inf[lab]['atom'])  #convert AOM freq to da/a
+        labs_data[lab].alphnorm(atom=par.inf[lab]['atom'])  #convert AOM freq to da/a
         #sd[lnum[lab]]=d[lab].std()
 
 def calc_for_single_mjd(p):
