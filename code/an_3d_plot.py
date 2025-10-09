9abca2d079ce58dc1e353d30756d56addc26a25a
Piotr Morzyński <pmorzynski@umk.pl>
2025-10-09 12:06:19 +0200

    upgrade of an_3d_plot.py

--- code/an_3d_plot.py
+++ code/an_3d_plot.py
@@ -1,10 +1,13 @@
+"""Plot results from maxvs_*.npy files from an_3D.py"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 camp = 'c2'
+path = '../results/2023_08_no_ptb/'
 
-data = np.load('maxvs_'+camp+'.npy')
+data = np.load(f'{path}maxvs_{camp}.npy')
 plt.plot(data[:,0],data[:,1]*1e-18)
 plt.yscale('log')
 plt.grid()
-plt.savefig('maxvs_'+camp+'.png')
+plt.savefig(f'{path}maxvs_{camp}.png')
