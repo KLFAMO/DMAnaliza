b85e55994f1d17d54cbf775afb83a8afe76abee9
Piotr Morzyński <pmorzynski@umk.pl>
2025-10-23 11:15:09 +0200

    bugfix local_settings

--- code/local_settings.py
+++ code/local_settings.py
@@ -3,6 +3,6 @@ project_path_str = ""
 
 import pathlib as pa
 if project_path_str:
-    project_path = pa.Path(__file__).absolute().parents[2]
-else:
     project_path = pa.Path(project_path_str)
+else:
+    project_path = pa.Path(__file__).absolute().parents[2]
